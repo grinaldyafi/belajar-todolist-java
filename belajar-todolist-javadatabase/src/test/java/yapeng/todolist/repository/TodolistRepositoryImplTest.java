fc465c3c4ee5c6f4644f9540d3a34de62c392304
Grinaldy Yafi' Rasyad <46275481+grinaldyafi@users.noreply.github.com>
2022-09-29 09:57:12 +0700

    Add files via upload

---
+++ belajar-todolist-javadatabase/src/test/java/yapeng/todolist/repository/TodolistRepositoryImplTest.java
@@ -0,0 +1,56 @@
+package yapeng.todolist.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import entity.Todolist;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.TodoListRepository;
+import repository.TodoListRepositoryImpl;
+import util.DatabaseUtil;
+
+public class TodolistRepositoryImplTest {
+    private HikariDataSource dataSource;
+
+    private TodoListRepository todoListRepository;
+
+    @BeforeEach
+    void setUp(){
+        dataSource = DatabaseUtil.getDataSource();
+        todoListRepository = new TodoListRepositoryImpl(dataSource);
+    }
+
+    @Test
+    void testAdd(){
+        Todolist todolist = new Todolist();
+        todolist.setTodo("Rasyad");
+
+        todoListRepository.add(todolist);
+    }
+
+    @Test
+    void testRemove() {
+        System.out.println(todoListRepository.remove(1));
+        System.out.println(todoListRepository.remove(2));
+        System.out.println(todoListRepository.remove(3));
+        System.out.println(todoListRepository.remove(4));
+    }
+    @Test
+    void testGetAll() {
+        todoListRepository.add(new Todolist("yafi"));
+        todoListRepository.add(new Todolist("rasyad"));
+        todoListRepository.add(new Todolist("grinaldy"));
+
+        Todolist[] todolists = todoListRepository.getAll();
+        for (var todo : todolists) {
+            System.out.println(todo.getId() + " : " + todo.getTodo());
+        }
+    }
+
+    @AfterEach
+    void tearDown(){
+        dataSource.close();
+    }
+
+
+}
